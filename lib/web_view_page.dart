434adaab37a0281b3baa812e30fb0c296d1b655b
YishakAlemu <yishakalemu28@gmail.com>
2025-03-28 01:44:43 -0600

    adding new depenencies to the pubspec.yaml

---
+++ lib/web_view_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewPage extends StatefulWidget {
+  @override
+  _WebViewPageState createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    _webViewController = WebViewController()..setJavaScriptMode(JavaScriptMode.unrestricted);
+    _loadWebPage();
+  }
+
+  void _loadWebPage() {
+    _webViewController.loadRequest(Uri.parse('https://ethiojobs.net/jobs'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebViewWidget(controller: _webViewController),
+    );
+  }
+}
