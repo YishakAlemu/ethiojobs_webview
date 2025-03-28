92bb609d3453ef20f120bc5021d7e9873a7bea94
YishakAlemu <yishakalemu28@gmail.com>
2025-03-28 05:04:06 -0600

    creating a cutom button to navigate through pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,59 @@
- // Import the new file
-import 'package:ethiojobs_webview/web_view_page.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key});
+
+  @override
+  _WebViewPageState createState() => _WebViewPageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _webViewController;
+  bool _isLoading = true; // Track loading state
+
+  @override
+  void initState() {
+    super.initState();
+    _webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (url) {
+            setState(() {
+              _isLoading = false; // Page has finished loading
+            });
+          },
+        ),
+      );
+    _loadWebPage();
+  }
+
+  void _loadWebPage() {
+    _webViewController.loadRequest(Uri.parse('https://ethiojobs.net/jobs'));
+  }
+
+  Future<bool> _onWillPop() async {
+    if (await _webViewController.canGoBack()) {
+      await _webViewController.goBack();
+      return false; // Prevent the default back navigation
+    }
+    return true; // Allow the default back navigation
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'EthioJobs',
-      debugShowCheckedModeBanner: false,
-      home: WebViewPage(), // Use the new WebViewPage
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: Stack(
+          children: [
+            WebViewWidget(controller: _webViewController),
+            if (_isLoading) // Show loading indicator when loading
+              Center(child: CircularProgressIndicator()),
+          ],
+        ),
+      ),
     );
-  } 
+  }
 }
--- lib/web_view_page.dart
+++ lib/web_view_page.dart
@@ -10,11 +10,22 @@ class WebViewPage extends StatefulWidget {
 
 class _WebViewPageState extends State<WebViewPage> {
   late final WebViewController _webViewController;
+  bool _isLoading = true; // Track loading state
 
   @override
   void initState() {
     super.initState();
-    _webViewController = WebViewController()..setJavaScriptMode(JavaScriptMode.unrestricted);
+    _webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (url) {
+            setState(() {
+              _isLoading = false; // Page has finished loading
+            });
+          },
+        ),
+      );
     _loadWebPage();
   }
 
@@ -22,10 +33,34 @@ class _WebViewPageState extends State<WebViewPage> {
     _webViewController.loadRequest(Uri.parse('https://ethiojobs.net/jobs'));
   }
 
+  Future<void> _goBack() async {
+    if (await _webViewController.canGoBack()) {
+      await _webViewController.goBack();
+    } else {
+      // Optionally, you can navigate back to a different screen or exit the app
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WebViewWidget(controller: _webViewController),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: _webViewController),
+          if (_isLoading) // Show loading indicator when loading
+            Center(child: CircularProgressIndicator()),
+          Positioned(
+            top: 50, // Adjust the position as needed
+            left: 20, // Adjust the position as needed
+            child: FloatingActionButton(
+              onPressed: _goBack,
+              child: Icon(Icons.arrow_back),
+              tooltip: 'Back',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
